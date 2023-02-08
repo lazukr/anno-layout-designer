80b1b98073707262f6a9dc8895728e9a92746ca3
lazukr <peterh10201@gmail.com>
2023-02-07 21:14:48 -0800

    added a method to download the svg for now.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,19 +3,6 @@ import { MainMenu } from "./MainMenu";
 import { useState } from "react";
 
 export const App = () => {
-    const [width, setWidth] = useState(20);
-    const [height, setHeight] = useState(20);
-    const [selection, setSelection] = useState("cursor");
-
-    const setDimension = (width: number, height: number) => {
-        setWidth(width);
-        setHeight(height);
-    };
-
-    const updateSelection = (selection: string) => {
-        setSelection(selection);
-    }
-
     return (
         <div className="App">
             <MainMenu />
--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,23 +1,26 @@
-
+import "snapsvg-cjs";
+import SNAPSVG_TYPE from "snapsvg";
 import "bootstrap/dist/css/bootstrap.css";
+import { useEffect, useState } from "react";
+import { Editor } from "./Editor";
+import { Action } from "../editor/Cursor";
+import { getBuildingSelections, getCitizenSelections, getGame } from "../data/Series";
+
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
 import ToggleButton from "react-bootstrap/ToggleButton";
-import { Github, EraserFill, HandIndexFill } from "react-bootstrap-icons";
-import { getBuildingSelections, getCitizenSelections, getGame } from "../data/Series";
-import { useEffect, useState } from "react";
-import { Editor } from "./Editor";
-import { Action } from "../editor/Cursor";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
-interface MenuProps {
-};
+import { Github, EraserFill, HandIndexFill, FileEarmarkArrowDown, FileEarmarkArrowUp } from "react-bootstrap-icons";
+
+declare const Snap: typeof SNAPSVG_TYPE;
 
 const DEFAULT_GAME = "1800";
 
-export const MainMenu = ({
-}: MenuProps) => {
+export const MainMenu = () => {
     const [game, setGame] = useState(DEFAULT_GAME);
     const [gameData, setGameData] = useState(getGame(game));
     const [citizen, setCitizen] = useState(Object.values(gameData.citizens)[0]);
@@ -60,6 +63,28 @@ export const MainMenu = ({
                             <EraserFill color="red"/>
                         </ToggleButton>
                     </ToggleButtonGroup>
+                    <ButtonGroup>
+                        <Button variant="dark">
+                            <FileEarmarkArrowUp />
+                        </Button>
+                        <Button variant="dark"
+                            onClick={async event => {
+                                const result = Snap("#svg");
+                                const data = result.toDataURL();
+                                const blob = await (await fetch(data)).blob();
+                                var url = window.URL.createObjectURL(blob);
+                                const a = document.createElement("a");
+                                a.href = url;
+                                a.download = "anno-layout.svg";
+                                a.click();
+                                window.URL.revokeObjectURL(url);
+                                a.remove();
+                            }}
+                        >
+                            <FileEarmarkArrowDown />
+                        </Button>
+                    </ButtonGroup>
+
                     {getCitizenSelections({
                         citizens: Object.values(gameData.citizens),
                         defaultSelectValue: citizen.name,
